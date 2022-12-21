3b3e04fa0fa6992c33532aa9e2b9e406ad4e59e6
Dimeji <dimejiogunyoye@googlemail.com>
2022-12-21 00:41:47 +0000

    Day 16 - complete

--- src/main/java/com/github/aoc2022/dogunyoye/Day16.java
+++ src/main/java/com/github/aoc2022/dogunyoye/Day16.java
@@ -5,10 +5,67 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Day16 {
+
+    private static Map<Valve, List<String>> valvesMap;
+    private static Map<String, Valve> nameToValveMap;
+    private static Map<State, Integer> memo;
+
+    static class State {
+        String valveName;
+        Set<Valve> visited;
+        int timeLeft;
+        int numberOfPlayers;
+
+        State(String name, Set<Valve> visited, int timeLeft, int numberOfPlayers) {
+            this.valveName = name;
+            this.visited = visited;
+            this.timeLeft = timeLeft;
+            this.numberOfPlayers = numberOfPlayers;
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((valveName == null) ? 0 : valveName.hashCode());
+            result = prime * result + ((visited == null) ? 0 : visited.hashCode());
+            result = prime * result + timeLeft;
+            result = prime * result + numberOfPlayers;
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            State other = (State) obj;
+            if (valveName == null) {
+                if (other.valveName != null)
+                    return false;
+            } else if (!valveName.equals(other.valveName))
+                return false;
+            if (visited == null) {
+                if (other.visited != null)
+                    return false;
+            } else if (!visited.equals(other.visited))
+                return false;
+            if (timeLeft != other.timeLeft)
+                return false;
+            if (numberOfPlayers != other.numberOfPlayers)
+                return false;
+            return true;
+        }
+    }
 
     static class Valve {
         String name;
@@ -54,7 +111,8 @@ public class Day16 {
     }
 
     static Map<Valve, List<String>> parseValves(List<String> valves) {
-        final Map<Valve, List<String>> valvesMap = new HashMap<>();
+        valvesMap = new HashMap<>();
+        nameToValveMap = new HashMap<>();
 
         for (String valve : valves) {
             valve = valve.replaceAll(",", "")
@@ -70,15 +128,61 @@ public class Day16 {
                 children.add(parts[i]);
             }
 
-            valvesMap.put(new Valve(name, flowRate), children);
+            final Valve v = new Valve(name, flowRate);
+            valvesMap.put(v, children);
+            nameToValveMap.put(name, v);
         }
 
-        System.out.println(valvesMap);
         return valvesMap;
+    }
+
+    private static int traverseValves(String currentValve, Set<Valve> visited, int timeLeft, int numberOfPlayers) {
+        if (timeLeft == 0) {
+            if (numberOfPlayers == 2) {
+                return traverseValves("AA", visited, 26, numberOfPlayers - 1);
+            }
+            return 0;
+        }
+
+        final State state = new State(currentValve, visited, timeLeft, numberOfPlayers);
+        final Integer cached = memo.get(state);
+        if (cached != null && cached >= 0) {
+            return cached;
+        }
+
+        int result = 0;
+        final Valve valve = nameToValveMap.get(currentValve);
+        if (!visited.contains(valve) && valve.flowRate > 0) {
+            final Set<Valve> newVisited = new HashSet<>(visited);
+            newVisited.add(valve);
+            result = Math.max(result, ((timeLeft - 1) * valve.flowRate) + traverseValves(valve.name, newVisited, timeLeft - 1, numberOfPlayers));
+        }
+
+        for (final String child : valvesMap.get(valve)) {
+            result = Math.max(result, traverseValves(child, visited, timeLeft - 1, numberOfPlayers));
+        }
+
+        memo.put(state, result);
+        return result;
+    }
+
+    public static int findMaximumPressureIn30Mins() {
+        memo = new HashMap<>();
+        return traverseValves("AA", new HashSet<>(), 30, 1);
+    }
+
+    // takes a long time to complete
+    // TODO: Optimise
+    public static int findMaximumPressureWithElephant() {
+        memo = new HashMap<>();
+        return traverseValves("AA", new HashSet<>(), 26, 2);
     }
 
     public static void main(String[] args) throws IOException {
         final List<String> valves = Files.readAllLines(Path.of("src/main/resources/Day16.txt"));
         parseValves(valves);
+
+        System.out.println("Part 1: " + findMaximumPressureIn30Mins());
+        System.out.println("Part 2: " + findMaximumPressureWithElephant());
     }
 }
--- src/test/java/com/github/aoc2022/dogunyoye/Day16Test.java
+++ src/test/java/com/github/aoc2022/dogunyoye/Day16Test.java
@@ -1,5 +1,31 @@
 package com.github.aoc2022.dogunyoye;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class Day16Test {
-    
+    private static List<String> valves;
+
+    @BeforeClass
+    public static void setUp() throws IOException {
+        valves = Files.readAllLines(Path.of("src/test/resources/Day16TestInput.txt"));
+        Day16.parseValves(valves);
+    }
+
+    @Test
+    public void testPartOne() throws IOException {
+        assertEquals(1651, Day16.findMaximumPressureIn30Mins());
+    }
+
+    @Test
+    public void testPartTwo() throws IOException {
+        assertEquals(1707, Day16.findMaximumPressureWithElephant());
+    }
 }
